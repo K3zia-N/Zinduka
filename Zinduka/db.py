c87978695f6ce1f3de4a685b5c1755aba0bf2d71
craignotsanjay <craigkairo@gmail.com>
2025-07-21 12:59:04 +0300

    Update db.py

--- Zinduka/db.py
+++ Zinduka/db.py
@@ -1,11 +1,19 @@
-import sqlite3
+# import sqlite3
 import pymysql
 
 def get_db_connection():
- conn = pymysql.connect(
-  host="localhost",
-  user="root",
-  password="",
-  database="Zinduka"
- )
- return conn
+ conn: None
+ try:
+  conn = pymysql.connect(
+   host="localhost",
+   user="root",
+   password="",
+   database="Zinduka"
+  )
+  return conn
+except pymysql.Error as e:
+ # error handling
+ return None
+except Exception as e:
+ print(f"An unexpected error occurred: {e}")
+ return None
